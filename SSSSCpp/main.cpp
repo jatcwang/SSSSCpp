74e1ceb62bd28e999b8c0654c9eb65f14d64628c
Jacob Wang <ran6251438@hotmail.com>
2013-08-27 16:32:26 +1200

    removed test code

--- SSSSCpp/main.cpp
+++ SSSSCpp/main.cpp
@@ -18,22 +18,6 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	GF256init();
 	srand(clock());
-	//vector<pair<UINT, UINT>> lol = encodeByte(16, 5, 3);
-	//UINT result = decodeByte(lol); //result == 15
-	while (1) {
-		time_t num = rand() % 255 + 1;
-		vector<pair<UINT, UINT>> lol = encodeByte(num, 5, 3);
-		vector<pair<UINT, UINT>> lol2(lol.begin(), lol.begin() + 3);
-		UINT result = decodeByte(lol2);
-		std::cout << num << "\t" << result << std::endl;
-	}
-	//for (int num = 1; num < 256; ++num) {
-	//	vector<pair<UINT, UINT>> lol = encodeByte(num, 5, 3);
-	//	vector<pair<UINT, UINT>> lol2(lol.begin(), lol.begin() + 3);
-	//	UINT result = decodeByte(lol2);
-	//	if (result != num)
-	//		std::cout << num << "\t" << result << std::endl;
-	//}
 	return 0;
 }
 
