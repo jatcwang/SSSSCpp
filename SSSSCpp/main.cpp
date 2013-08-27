24fddc5e7df2c9b539ed88751c73037ac8a8ba33
Jacob Wang <ran6251438@hotmail.com>
2013-08-27 16:31:21 +1200

    testing code complete. no errors

--- SSSSCpp/main.cpp
+++ SSSSCpp/main.cpp
@@ -17,8 +17,23 @@ using std::make_pair;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	GF256init();
-	vector<pair<UINT, UINT>> lol = encodeByte(15, 5, 3);
-	UINT result = decodeByte(lol); //result == 15
+	srand(clock());
+	//vector<pair<UINT, UINT>> lol = encodeByte(16, 5, 3);
+	//UINT result = decodeByte(lol); //result == 15
+	while (1) {
+		time_t num = rand() % 255 + 1;
+		vector<pair<UINT, UINT>> lol = encodeByte(num, 5, 3);
+		vector<pair<UINT, UINT>> lol2(lol.begin(), lol.begin() + 3);
+		UINT result = decodeByte(lol2);
+		std::cout << num << "\t" << result << std::endl;
+	}
+	//for (int num = 1; num < 256; ++num) {
+	//	vector<pair<UINT, UINT>> lol = encodeByte(num, 5, 3);
+	//	vector<pair<UINT, UINT>> lol2(lol.begin(), lol.begin() + 3);
+	//	UINT result = decodeByte(lol2);
+	//	if (result != num)
+	//		std::cout << num << "\t" << result << std::endl;
+	//}
 	return 0;
 }
 
