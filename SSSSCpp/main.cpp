bad77b4503470750bf3213d5f5d2ee4e24da351e
Jacob Wang <ran6251438@hotmail.com>
2013-08-29 15:57:25 +1200

    Can react (not correctly yet) to command line arguments
    refs #9

--- SSSSCpp/main.cpp
+++ SSSSCpp/main.cpp
@@ -11,39 +11,84 @@
 #include <iomanip>
 #include <time.h>
 #include "boost/filesystem.hpp"
+#include "boost/program_options.hpp"
 
 #include <assert.h>
 #define UINT unsigned int
 
-using std::vector; using std::pair;
-using std::make_pair;
-using std::ifstream; using std::ofstream;
-using std::ios; using std::string;
-using std::cout; using std::endl;
-int _tmain(int argc, _TCHAR* argv[])
+namespace 
+{ 
+	const size_t ERROR_IN_COMMAND_LINE = 1; 
+	const size_t SUCCESS = 0; 
+	const size_t ERROR_UNHANDLED_EXCEPTION = 2; 
+
+} // namespace 
+
+using std::vector; using std::string;
+using boost::filesystem::path;
+
+int main(int argc, char* argv[])
 {
 	GF256init();
 	srand(time(NULL));
-	int n = 5;
-	int k = 3;
-	string folder("../../test/");
-	string inputFileName("input.jpg");
-	string reconstructFileName("output.jpg");
-	boost::filesystem::path inputFile(folder + inputFileName);
-	boost::filesystem::path outputPath(folder + reconstructFileName);
-	vector<boost::filesystem::path> shareFiles(n);
-	for (int i = 0; i < n; ++i) {
-		std::stringstream ss;
-		ss << std::setw(3) << std::setfill('0') << i + 1;
-		shareFiles[i] = inputFile.string() + "." + ss.str();
-	}
-	cout << "Splitting" <<endl;
-	splitSecretFile(inputFile, n, k);
 
-	//remove 2 files
-	shareFiles.erase(shareFiles.begin(), shareFiles.begin() + 2);
-	cout << "Reconstructing" << endl;
-	reconstructSecretFile(shareFiles, outputPath);
-	
+	try 
+	{ 
+		/** Define and parse the program options 
+		*/ 
+		namespace po = boost::program_options; 
+		po::options_description desc("Program Usage"); 
+		desc.add_options() 
+			("help,h", "Print help messages") 
+			("split,s", po::value<vector<string>>()->multitoken(), "Split a file");
+		//("reconstruct,r", po::value<vector<string>>(&reconInputPaths), "Reconstruct a file from its shares");
+		//("options,o", "additional options") 
+
+		po::variables_map vm; 
+		try 
+		{ 
+			po::store(po::parse_command_line(argc, argv, desc),
+				vm); // throws on error 
+
+			/** --help option 
+			*/ 
+			if (vm.count("help")) 
+			{ 
+				std::cout << "Basic Command Line Parameter App" << std::endl 
+					<< desc << std::endl; 
+				return SUCCESS; 
+			} 
+
+			if (vm.count("split")) {
+				std::cout << "Splitting" << std::endl;
+				vector<string> const &splitInputs = vm["split"].as<vector<string>>();
+				path inputPath(splitInputs[0]);
+				int n = atoi(splitInputs[1].c_str());
+				int k = atoi(splitInputs[2].c_str());
+				return SUCCESS;
+			}
+
+			po::notify(vm); // throws on error, so do after help in case there are any problems 
+		} 
+		catch(po::error& e) 
+		{ 
+			std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
+			std::cerr << desc << std::endl; 
+			return ERROR_IN_COMMAND_LINE; 
+		} 
+
+		// application code here // 
+
+	} 
+	catch(std::exception& e) 
+	{ 
+		std::cerr << "Unhandled Exception reached the top of main: " 
+			<< e.what() << ", application will now exit" << std::endl; 
+		return ERROR_UNHANDLED_EXCEPTION; 
+
+	} 
+
+	return SUCCESS; 
+
 	return 0;
 }
