3ec0143a374e35921c2f90b53142a607eb1aa01e
Jacob Wang <ran6251438@hotmail.com>
2013-08-29 17:14:48 +1200

    splitting: added lexical casting for n and k, and corresponding error handling
    refs #9

--- SSSSCpp/main.cpp
+++ SSSSCpp/main.cpp
@@ -12,6 +12,7 @@
 #include <time.h>
 #include "boost/filesystem.hpp"
 #include "boost/program_options.hpp"
+#include "boost/lexical_cast.hpp"
 
 #include <assert.h>
 #define UINT unsigned int
@@ -40,7 +41,8 @@ int main(int argc, char* argv[])
 		po::options_description desc("Program Usage"); 
 		desc.add_options() 
 			("help,h", "Print help messages") 
-			("split,s", po::value<vector<string>>()->multitoken(), "Split a file");
+			("split,s", po::value<vector<string>>()->multitoken(), "Split a file")
+			("reconstruct,r", po::value<vector<string>>()->multitoken(), "Reconstruct a file from its shares");
 		//("reconstruct,r", po::value<vector<string>>(&reconInputPaths), "Reconstruct a file from its shares");
 		//("options,o", "additional options") 
 
@@ -63,24 +65,42 @@ int main(int argc, char* argv[])
 				std::cout << "Splitting" << std::endl;
 				vector<string> const &splitInputs = vm["split"].as<vector<string>>();
 				if (splitInputs.size() != 3) {
-					throw po::error("Split usage: [Input File] [Total Share #] [Share # Required to Reconstruct]");
+					throw po::error("Split usage: [Input File] [Total Share #] [Share Number Required to Reconstruct]");
 				}
 				path inputPath(splitInputs[0]);
-				int n = atoi(splitInputs[1].c_str());
-				int k = atoi(splitInputs[2].c_str());
-				if (n <= 0 || k <= 0 || n > 255 || k > 255) {
-					throw po::error("n and k must be between 1 and 255 (inclusive)");
-				}
-				if (n < k) {
-					throw po::error("n must be larger or equal to k");
-				}
 				try {
+					int n = boost::lexical_cast<int>(splitInputs[1].c_str());
+					int k = boost::lexical_cast<int>(splitInputs[2].c_str());
+					if (n <= 0 || k <= 0 || n > 255 || k > 255) {
+						throw po::error("n and k must be between 1 and 255 (inclusive)");
+					}
+					if (n < k) {
+						throw po::error("n must be larger or equal to k");
+					}
 					splitSecretFile(inputPath, n, k);
+				}
+				catch (boost::bad_any_cast &e) {
+					throw po::error("[Total Share Number] and [Share Number Required to Reconstruct] must be numbers");
 				}
 				catch (std::exception &e) {
 					throw po::error(e.what());
 				}
 				return SUCCESS;
+			}
+
+			if (vm.count("reconstruct")) {
+				std::cout << "Reconstructing" << std::endl;
+				vector<string> const &reconInputs = vm["reconstruct"].as<vector<string>>();
+				if (reconInputs.size() != 2)
+					throw po::error("Reconstruct Usage: [Original File Name (and the folder path containing the shares)] [Output File Name (and folder path)]");
+				path inputFile(reconInputs[0]);
+				path outputFile(reconInputs[1]);
+				try {
+					reconstructSecretFile(inputFile, outputFile);
+				}
+				catch (std::exception &e) {
+					throw po::error(e.what());
+				}
 			}
 
 			po::notify(vm); // throws on error, so do after help in case there are any problems 
